0ab62ca9a13d8fa30006840cb964e7b779569e55
YuliaEka1 <90075970+YuliaEka1@users.noreply.github.com>
2022-03-03 21:24:58 +0700

    controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return "2041720064 <br> Yulia Eka Ardhani <br> TI2C";
+    }
+    //
+}
